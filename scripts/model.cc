5714d07945899549b4c8df1b58d74dba55443ea2
pAplakidis <user.email=aplakidisp@gmail.com>
2021-11-01 18:19:26 +0200

    opencv works

--- scripts/model.cc
+++ scripts/model.cc
@@ -1,11 +1,3 @@
-#include <iostream>
-#include <memory>
-#include <stdlib.h>
-
-#include <torch/torch.h>
-#include <torch/script.h>
-#include <opencv2/opencv.hpp>
-
 #include "model.h"
 
 torch::jit::script::Module load_model(){
@@ -22,7 +14,16 @@ torch::jit::script::Module load_model(){
   return module;
 }
 
-void classify(){
+cv::Mat load_image(std::string img_path){
+  cv::Mat img = cv::imread(img_path, 0);
+  cv::imshow("grayscale image", img);
+  cv::waitKey(0);
+  cv::destroyAllWindows();
+
+  return img;
+}
+
+std::string classify(){
 
 }
 
@@ -31,7 +32,9 @@ int main(int argc, char** argv){
     std::cerr << "Usage: " << argv[0] << " <image_path>\n";
     return -1;
   }
+  std::string img_path = argv[1];
 
-  load_model();
+  //load_model();
+  cv::Mat img = load_image(img_path);
 }
 
--- scripts/model.h
+++ scripts/model.h
@@ -1,5 +1,14 @@
+#include <iostream>
+#include <memory>
+#include <stdlib.h>
+
+#include <torch/torch.h>
+#include <torch/script.h>
+#include <opencv2/opencv.hpp>
+
 std::string model_path = "../models/traced_simple_classifier.pt";
 
 torch::jit::script::Module load_model();
-void classify();
+cv::Mat load_image(std::string img_path);
+std::string classify();
 
