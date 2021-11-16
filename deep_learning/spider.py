f1adee0f0e436e1130740731bd53874a8fd48a86
pAplakidis <user.email=aplakidisp@gmail.com>
2021-11-16 20:00:35 +0200

    started working on web crawler

--- deep_learning/spider.py
+++ deep_learning/spider.py
@@ -1,5 +1,10 @@
 import threading
+import json
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 from urllib.parse import urlparse, parse_qs
+
+class Spider:
+  def __init__(self):
+    self.current_food = None
 
